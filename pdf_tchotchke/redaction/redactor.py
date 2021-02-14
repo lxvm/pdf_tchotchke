e400a97246f3c58e8ab45992d1f034768f67d664
lord-zo <lorenzo@vanmunoz.com>
2021-02-13 17:37:01 -0800

    touch redactor

---
+++ pdf_tchotchke/redaction/redactor.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+# redactor.py
+# Author: Lorenzo Van Munoz
+# Last updated: 13/02/2021
+
+'''
+This module implements text search and replace
+algorithms using pdftotext on pdf_redactor objects.
+The functions written here are called by redact.py
+for the command line interface.
+'''
+
+import pdftotext
+
+from .editor import *
+
+## Not implemented
+# See whiteout and whiteout_re for inspiration
+
+if __name__=='__main__':
+    raise SystemExit()
